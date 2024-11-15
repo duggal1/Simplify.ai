7e74ad79f187f3118f5bbed167696f57f8f94e2f
harshit duggal <135515880+duggal1@users.noreply.github.com>
2024-11-15 11:05:08 +0530

    Update errors.ts

--- types/errors.ts
+++ types/errors.ts
@@ -1,47 +1,13 @@
 export class YouTubeError extends Error {
-  public statusCode: number;
-  public errorCode?: string;
-
-  constructor(message: string, statusCode: number = 500, errorCode?: string) {
+  constructor(message: string) {
     super(message);
     this.name = 'YouTubeError';
-    this.statusCode = statusCode;
-    this.errorCode = errorCode;
-
-    // Set the prototype explicitly when extending Error in TypeScript
-    Object.setPrototypeOf(this, YouTubeError.prototype);
-  }
-
-  public toJSON() {
-    return {
-      name: this.name,
-      message: this.message,
-      statusCode: this.statusCode,
-      errorCode: this.errorCode,
-    };
   }
 }
 
 export class GeminiError extends Error {
-  public statusCode: number;
-  public errorCode?: string;
-
-  constructor(message: string, statusCode: number = 500, errorCode?: string) {
+  constructor(message: string) {
     super(message);
     this.name = 'GeminiError';
-    this.statusCode = statusCode;
-    this.errorCode = errorCode;
-
-    // Set the prototype explicitly when extending Error in TypeScript
-    Object.setPrototypeOf(this, GeminiError.prototype);
   }
-
-  public toJSON() {
-    return {
-      name: this.name,
-      message: this.message,
-      statusCode: this.statusCode,
-      errorCode: this.errorCode,
-    };
-  }
-}
+} 
